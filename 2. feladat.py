eda2d6ade343d85d3b8ac3f439d62f2894775c9e
LiwChandler <64069934+LiwChandler@users.noreply.github.com>
2020-04-22 18:15:36 +0200

    Add files via upload

---
+++ 2. feladat.py
@@ -0,0 +1,36 @@
+import numpy as np
+while True:
+    try:
+        dbszam=int(input("A pénzösszegek darabszáma: "))
+        if dbszam<1 or dbszam>1000:
+            raise
+        break
+    except:
+        print("Egy 1 és 1000 közötti számot adjon meg!")
+def kiiras(a):
+    for i in range(a):
+        while True:
+            try:
+                szam=int(input(f"Adja meg a(z) {i}. pénzösszeget: "))
+                if szam<1 or szam>100000:
+                    raise
+                print(eredmeny(szam,ermek))
+                break
+            except:
+                print("Adjon meg egy 1 és 100000 közötti számot.")
+def eredmeny(b,c):
+    c=np.asarray(c)
+    ermek_sorrendben=np.sort(c)[::-1]
+    vegeredmeny=""
+    flag=0
+    for i in ermek_sorrendben:
+        if b//i>=1 and flag==1:
+            vegeredmeny+=f";{b//i}db {i}Ft"
+            b=b-(b//i)*i
+        if b//i>=1 and flag==0:
+            vegeredmeny+=f"{b//i}db {i}Ft"
+            b=b-(b//i)*i
+            flag+=1
+    return vegeredmeny
+ermek=[1,2,5,10,50,100]
+kiiras(dbszam)
