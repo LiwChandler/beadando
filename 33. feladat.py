b1f9f46a7e3dd9e5a399f31cdfd6f47d119e0edf
LiwChandler <64069934+LiwChandler@users.noreply.github.com>
2020-04-23 18:52:00 +0200

    javított változat

--- 33. feladat.py
+++ 33. feladat.py
@@ -11,22 +11,37 @@ def helyes():
             if "@" not in cim:
                 print("A megadott email cím nem tartalmaz '@'-ot!")
                 continue
-            if cim[cim.find("@"):].find(".")==-1:
+            domain=cim[cim.rfind("@"):]
+            if domain.find(".")==-1:
                 print("A megadott e-mail cím vége nem megfelelő (pl.: '.hu', '.com')!")
                 continue
-            if cim[cim.rfind("."):] not in domainveg:
+            if domain[domain.rfind("."):] not in domainveg:
                 print("A megadott domain vége nem engedélyezett!")
+                continue
+            if cim[cim.rfind("@")+1]!=cim[cim.rfind("@")+1].lower():
+                print("A domain első betűje nem lehet nagybetű!")
                 continue
             if cim.rfind("@")==0:
                 print("Az e-mail cím nem tartalmaz azonosítót!")
                 continue
-            if cim[cim.find("@")+1]==cim[cim.find("@")+1].capitalize():
-                print("A domain első betűje nem lehet nagybetű!")
-                continue
-            for i in cim:
+
+            flag1=None
+            flag2=None
+            for i in cim[:cim.rfind("@")+1]:
                 if i in tiltott_karakterek:
-                    print("Az email cím tiltott karaktert tartalmaz!")
-                    continue
+                    flag1=True
+            for i in domain:
+                if i in tiltott_karakterek:
+                    flag2=True
+            if flag1 is True and flag2 is None:
+                print("Az azonosító tiltott karaktert tartalmaz!")
+                continue
+            if flag1 is None and flag2 is True:
+                print("A domain tiltott karaktert tartalmaz!")
+                continue
+            if flag1 is True and flag2 is True:
+                print("Az azonosító és a domain is tartalmaz tiltott karaktert!")
+                continue
             print("A megadott e-mail cím helyes!")
         except:
             print("Helyes e-mail címet adjon meg!")
