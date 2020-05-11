2d882ec5ee5db6337bcd57c74b7c72e555d571a0
LiwChandler <64069934+LiwChandler@users.noreply.github.com>
2020-05-11 18:10:18 +0200

    Update 35. feladat.py

--- 35. feladat.py
+++ 35. feladat.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 
 def mutat(ls):
+    if ls==['']:
+        return "Az ön listája üres."
     elemek=[]
     elofordulas=[]
     ls=np.asarray(ls)
@@ -11,8 +13,19 @@ def mutat(ls):
         if i not in elemek:
             elemek.append(i)
             elofordulas.append(np.sum(ls==i))
-    plt.bar(elemek,elofordulas)
+    plt.bar(elemek,elofordulas,color="g")
     plt.title("Elemek előfordulása a listában")
     plt.xlabel("Elemek")
     plt.ylabel("Előfordulás száma")
     plt.show()
+    if max(elofordulas)>len(ls)/2:
+        for i in range(len(elofordulas)):
+            if elofordulas[i]==max(elofordulas):
+                return f"{elemek[i]} ({max(elofordulas)}x szerepel egy {len(ls)} hosszú listában)"
+    return ""
+
+while True:
+    a=list(input("Adja meg egy lista elemeit vesszővel elválasztva (stop beírása esetén kilép a programból: ").split(","))
+    if a==['stop']:
+        break
+    print(mutat(a))
